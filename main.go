49d5fe08d6f6e093633e68b1784e64cacc12aca3
Fabio Leitao <fabio.leitao@ictsirio.com>
2023-09-28 18:40:47 -0300

    Respondendo json de dados mockup

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/FabioLeitao/go-rest-api/models"
 	"github.com/FabioLeitao/go-rest-api/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor REST com Go")
 	routes.HandleRequest()
 
