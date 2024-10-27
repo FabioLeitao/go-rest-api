6429b8ea2b2b82da15ba31520122ee241d1a051e
Fabio Leitao <fabio.tleitao@gmail.com>
2024-10-26 22:46:35 -0300

    Configurado para usar Gorilla Mux como router

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,14 @@ package routes
 import (
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 
 	"github.com/FabioLeitao/go-rest-api/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPesonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPesonalidades)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
