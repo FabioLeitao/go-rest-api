c5d614e58fc6a3bf9aeec0b2f7f51fa473768839
Fabio Leitao <fabio.tleitao@gmail.com>
2024-10-26 22:29:49 -0300

    Atualizando versao do go para compatibilidade

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/FabioLeitao/go-rest-api
 
-go 1.21.1
+go 1.23
--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	models.Personalidades = []models.Personalidade{
 		{Nome: "Nome 1", Historia: "Historia 1"},
 		{Nome: "Nome 2", Historia: "Historia 2"},
+		{Nome: "Nome 3", Historia: "Historia 3"},
+		{Nome: "Nome 4", Historia: "Historia 4"},
 	}
 
 	fmt.Println("Iniciando o servidor REST com Go")
