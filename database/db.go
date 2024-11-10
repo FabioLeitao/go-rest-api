e681d7ac345858da6f9f83eea9b70ac52fe0b8b7
Fabio Leitao <fabio.tleitao@gmail.com>
2024-11-09 23:00:15 -0300

    configuracoes para conectar ao PG

--- database/db.go
+++ database/db.go
@@ -5,14 +5,14 @@ import (
 	  "gorm.io/gorm"
   )
 
-var {
+var (
 	DB *gorm.DB
 	err error
-}
+)
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao)
+	strDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(strDeConexao)
 	if err != nil {
 		log.Panic("Erro ao conectar com Banco de Dados")
 	}
